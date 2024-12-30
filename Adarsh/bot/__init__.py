94eb4faa3cafb9585e6d6a8fe452e33755bd7732
oxnet_ir <96020775+adoxnet@users.noreply.github.com>
2024-12-30 16:22:50 +0330

    Update __init__.py

--- Adarsh/bot/__init__.py
+++ Adarsh/bot/__init__.py
@@ -6,9 +6,9 @@ from os import getcwd
 
 StreamBot = Client(
     name='Web Streamer',
-    api_id=Var.API_ID,
-    api_hash=Var.API_HASH,
-    bot_token=Var.BOT_TOKEN,
+    api_id=Var.21675188,
+    api_hash=Var.e0a522628a13877a441327362b0cf431,
+    bot_token=Var.6721837633:AAGEdUG9K5AhWdpro7z-ZXVJed-yJ_HW6tM,
     sleep_threshold=Var.SLEEP_THRESHOLD,
     workers=Var.WORKERS
 )
